66961a8942688f44e7779a46a3da40e1c00d1565
iSanyok <supergulman09@gmail.com>
2021-03-03 15:29:09 +0300

    Count by X

---
+++ task8.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Create a function with two arguments that will return an array of the first (n) multiples of (x).
+ * Assume both the given number and the number of times to count will be positive numbers greater than 0.
+ * Return the results as an array
+ *
+ * @param int $n
+ * @param int $x
+ * @return array
+ */
+function foo(int $n, int $x): array
+{
+    $result = [];
+
+    for($i = 1; $i <= $x; $i++) {
+        $result[] = $n * $i;
+    }
+
+    return $result;
+}
+
+print_r(foo(2, 5));

3fba041595cc1c9b06e8a1629c7d4a3941512d4c
iSanyok <supergulman09@gmail.com>
2021-03-04 16:23:19 +0300

    Double Trouble fix

--- tasks1-10/task2.php
+++ tasks1-10/task2.php
@@ -2,6 +2,7 @@
 /**
  * Given an array of integers (x), and a target (t), you must find out if any two consecutive numbers in the array sum to t.
  * If so, remove the second number.
+ *
  * Example:
  * x = [1, 2, 3, 4, 5]
  * t = 3
@@ -9,21 +10,20 @@
  * [1, 3, 4, 5]
  * Work through the array from left to right.
  * Return the resulting array.
+ *
+ * @param int $t
  * @param array $x
  * @return array
  */
 function foo(array $x, int $t): array
 {
-    foreach ($x as $key => $value)
-    {
-        if($t == ($x[$key] + $x[$key + 1]))
-        {
-            unset($x[$key + 1]);
-            return $x;
+    for($i = 0; $i <= count($x); $i++) {
+        if($t == ($x[$i] + $x[$i + 1])) {
+            array_splice($x, $i + 1, 1);
+            $i--;
         }
     }
-
     return $x;
 }
 
-print_r(foo([1, 2, 3, 4, 5], 3));
+print_r(foo([41231, 13213123, 111111, 133232, 44343434, 5454566, 5454566, 123123, 443434355, 62451, 3232, 6463333, 1212333333, 43451231], 10909132));
